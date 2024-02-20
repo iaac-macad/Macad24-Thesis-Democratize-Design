58007b9ba0aadb3a636daed0fd70c871c168af25
David Leon <david.andres.leon@iaac.net>
2024-02-20 09:23:11 +0100

    file3dm input

---
+++ src/App.vue
@@ -0,0 +1,188 @@
+<!-- // Script is in some way equivalent to script.js. This is place
+to define variables, methods and imports of other Vue compoennts. -->
+<script setup>
+// Understanding ref article: https://blog.logrocket.com/understanding-vue-refs/#:~:text=Ref%20s%20are%20Vue.,element%20in%20your%20Vue%20instance.
+// When ref attribute is added to element, this element then can be referenced
+// in template. It is sort of templatecement of getElementById (but better)
+import { ref, onBeforeMount, computed } from "vue";
+import { loadRhino } from "@/scripts/compute.js";
+
+// Import other Vue components in order to add them to a template.
+import Header from "./components/Header.vue"
+import GeometryView from "./components/GeometryView.vue"
+import SliderInput from "./components/SliderInput.vue"
+import Upload3dm from "./components/Upload3dm.vue"
+
+
+import def from './assets/rhino_input.gh' //import Grasshopper definition for assets
+
+let firstSliderName = ref("Radius") //must match the Input name in your GH definition!
+let firstSliderValue = ref(10) //default slider value
+
+let secondSliderName = ref("Count") //must match the Input name in your GH definition!
+let secondSliderValue = ref(10) //default slider value
+
+const encodedFile = ref(null);
+
+///.............................................
+let path = def //path to the Grasshopper definition
+let data = ref({})
+let metadata = ref([])
+
+
+function updateValue(newValue, parameterName) {
+  console.log(parameterName)
+
+  if (parameterName === firstSliderName.value) {
+    firstSliderValue.value = newValue
+  } 
+  
+  else if (parameterName === secondSliderName.value) {
+    secondSliderValue.value = newValue
+    console.log(secondSliderValue.value)
+  }
+  
+}
+
+function update3dmData(newData) {
+  encodedFile.value = newData;
+}
+
+
+//receive metadata from GeometryView component
+function receiveMetedata(newMetadata) {
+  console.log(newMetadata);
+  metadata.value = newMetadata;
+}
+
+// a computed ref. Vue will keep track of this and update it
+const computeData = computed(() => {
+  data = {
+    ["encodedFile"]: encodedFile.value, 
+    [firstSliderName.value]: Number(firstSliderValue.value),
+    [secondSliderName.value]: Number(secondSliderValue.value)
+  };
+
+  return data
+})
+
+onBeforeMount( () => {
+})
+
+</script>
+
+<!-- Template is a HTML-based syntax that allows you to bind the rendered DOM elements
+with data, objects, functions etc. -->
+<template>
+  <Header title="Digital Tools for Cloud-based Data Management - Session 04 - Compute"></Header>
+
+  <div id="content">
+    <div id="sidebar" class="container">
+
+      <Upload3dm @encoded3dm="update3dmData"/>
+      
+        
+      <SliderInput 
+          :title="firstSliderName"
+          :min = "1"
+          :max = "10"
+          :step = "1"
+          :val = "5"
+          @update="updateValue"></SliderInput>
+
+          <SliderInput 
+          :title="secondSliderName"
+          :min = "1"
+          :max = "10"
+          :step = "1"
+          :val = "5"
+          @update="updateValue"></SliderInput>
+
+
+    </div>
+      
+    <div id="viewer" class="container">
+
+      <GeometryView
+      :data="computeData"
+      :path="path"
+      @updateMetadata="receiveMetedata"
+      />
+
+    </div>
+  </div>
+
+</template>
+
+<!-- Style is for CSS styling -->
+<style>
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+
+      
+  background-color: rgb(59, 59, 59); 
+  color: white;
+}
+
+
+.logo-image {
+  height: 3.25rem;
+  padding: 0.5rem;
+}
+
+h2 {
+  font-size: 1.125rem;
+  font-weight: 600;
+  letter-spacing: 0.01em;
+}
+
+#content{
+    
+    display: flex;
+    height: calc(100vh - 68px);
+}
+
+#sidebar{
+    
+    width:310px;
+    padding: 10px;
+}
+
+#viewer{
+    width: calc(100% - 310px);
+}
+
+
+.container{
+
+    border-style: dotted;
+    border-width: 1px;
+
+}
+
+.modern-range {
+  -webkit-appearance: none;
+  width: 100%;
+  background: linear-gradient(90deg, #ffffff, #ff0080);
+  height: 17px;
+  border-radius: 15px;
+  margin: 10px 0px;
+}
+
+.modern-range::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  height: 15px;
+  width: 15px;
+  border-radius: 15px;
+  background-color: black;
+  cursor: pointer;
+}
+
+
+</style>
+
---
+++ src/components/Upload3dm.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+
+        <input type="file" @change="handleFileUpload" />
+        <button v-if="selectedFile" @click="uploadFile">Upload File</button>
+
+
+        <!-- <div class="button">
+            <label for='upload3dm' class='customUpload'
+              title="Upload the preprocessed .3dm model to calculate the coverage of the sensors">Upload 3dm</label>
+            <input type="file" accept=".3dm" id="upload3dm" hidden />
+            <span id="3dm-chosen" class="fileText">No file chosen</span>
+          </div> -->
+
+    </div>
+</template>
+  
+<script setup>
+import { ref } from 'vue';
+import { base64ByteArray } from "@/scripts/compute.js";
+
+let uploadDoc = ref(null);
+
+const emits = defineEmits(['encoded3dm']);
+
+const selectedFile = ref(null);
+
+const handleFileUpload = event => {
+    selectedFile.value = event.target.files[0];
+};
+
+const uploadFile = async () => {
+    if (!selectedFile.value) {
+        return;
+    }
+
+
+    // get file
+    let  file = selectedFile.value
+
+    // try to open 3dm file
+    const buffer3dm = await file.arrayBuffer()
+    const arr = new Uint8Array(buffer3dm)
+    const b64ba = base64ByteArray(arr)
+
+    uploadDoc.value = b64ba
+
+    emits('encoded3dm', uploadDoc.value);
+
+    // selectedFile.value = null;
+
+}
+</script>
+  
+<style scoped>
+/* Add styles here if needed */
+</style>
